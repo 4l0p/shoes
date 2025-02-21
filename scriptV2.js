736702c5f0a252beb26cc1dc3ec365eb3f12524b
André L de O Pereira <alop.mail@gmail.com>
2025-02-21 14:38:02 -0300

    ajustes v2 javascript e css

--- scriptV2.js
+++ scriptV2.js
@@ -37,3 +37,56 @@ thumbnails.forEach((thumbnail, index) => {
 });
 
 showCurrentImage();
+
+const mainPhoto = document.querySelector('.main-photo')
+const optionsColorsImg = document.querySelector('.options--colors__img img')
+const bigPhotos = ['black', 'grey', 'orange', 'red']
+const optionSize = document.querySelectorAll('.size')
+currentImageIndex = 0
+
+//mostra a imagem do tenis preto por padrão
+mainPhoto.innerHTML = `<img src="./assets/${bigPhotos[currentImageIndex]}-big.jpg" alt="Imagem Grande do Tênis"></img>`
+
+//seleciona o tenis preto por padrão
+optionsColorsImg.classList.add('selected')
+
+
+// remove qualquer seleção de tamanhos ao carregar a pagina
+optionSize.forEach((size) => {
+  size.classList.remove('selected')
+})
+
+// função para selecionar o tamanho
+optionSize.forEach((size) => {
+  size.addEventListener('click', (e) => {
+    // Verifica se o elemento clicado já está selecionado
+    if (e.target.classList.contains('selected')) {
+      // Se já estiver selecionado, remove a seleção
+      e.target.classList.remove('selected');
+    } else {
+      // Remove a classe 'selected' de todos os elementos
+      optionSize.forEach((otherSize) => {
+        otherSize.classList.remove('selected');
+      });
+      e.target.classList.add('selected')
+    }
+  })
+})
+
+
+
+
+function showMainPhoto() {
+  console.log(mainPhoto)
+  console.log(mainPhoto)
+  optionsColorsImg.addEventListener('click', () => {
+    bigPhotos.forEach((photo) => {
+      const tagPhoto = `<img src="./assets/${photo}-big.jpg" alt="Imagem Grande do Tênis"></img>`
+      const thumPhoto = `<img src="./assets/${photo}-thumb.jpg" alt="Preto Thumb">`
+      optionsColorsImg.innerHTML = thumPhoto
+      mainPhoto.innerHTML = tagPhoto
+    })
+  })
+}
+
+
