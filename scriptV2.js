e82546fb1fd2a09b2494be58f86a418122d66c5e
André L de O Pereira <alop.mail@gmail.com>
2025-03-08 12:36:40 -0300

    teste ajustes de seleção de cor, tamanho, quantidade e preço mais configuração do json

--- scriptV2.js
+++ scriptV2.js
@@ -3,6 +3,7 @@ const images = section6.querySelectorAll('.section__slide--shoe img');
 const thumbnails = section6.querySelectorAll('.thumb-circle');
 const prevButton = section6.querySelector('.section__slide--prev');
 const nextButton = section6.querySelector('.section__slide--next');
+let selectedSize, selectedQuantity, selectedColor, selectedPrice = ''
 
 let currentImageIndex = 0;
 
@@ -48,17 +49,17 @@ optionSize.forEach((size) => {
 // função para selecionar o tamanho
 optionSize.forEach((size) => {
   size.addEventListener('click', (e) => {
-    // Verifica se o elemento clicado já está selecionado
-    if (e.target.classList.contains('selected')) {
-      // Se já estiver selecionado, remove a seleção
-      e.target.classList.remove('selected');
-    } else {
+    // // Verifica se o elemento clicado já está selecionado
+    // if (e.target.classList.contains('selected')) {
+    //   // Se já estiver selecionado, remove a seleção
+    //   e.target.classList.remove('selected');
+    // } else {
       // Remove a classe 'selected' de todos os elementos
       optionSize.forEach((otherSize) => {
         otherSize.classList.remove('selected');
       });
       e.target.classList.add('selected')
-    }
+      selectedSize = size.innerHTML
   })
 })
 
@@ -82,13 +83,14 @@ mainPhoto.innerHTML = `<img src="./assets/${bigPhotos[currentImageIndex]}-big.jp
 titleProduct.innerHTML = `<h1>Xtreme 200mx - <span class='capitalize'>${colorProduct[currentImageIndex]} </span></h1>`
 //seleciona o tenis preto por padrão
 optionsColorsImg[currentImageIndex].classList.add('selected')
+selectedColor = colorProduct[currentImageIndex]
 
 optionsColorsImg.forEach((option, index) => {
   option.addEventListener('click', (e) => {
-    if (e.target.classList.contains('selected')) {
-      e.target.classList.remove('selected')
-      titleProduct.innerHTML = `<h1>Xtreme 200mx</h1>`
-    } else {
+    // if (e.target.classList.contains('selected')) {
+    //   e.target.classList.remove('selected')
+    //   titleProduct.innerHTML = `<h1>Xtreme 200mx</h1>`
+    // } else {
       optionsColorsImg.forEach((otherOptions) => {
         otherOptions.classList.remove('selected')
         titleProduct.innerHTML = `<h1>Xtreme 200mx</h1>`
@@ -96,15 +98,14 @@ optionsColorsImg.forEach((option, index) => {
       e.target.classList.add('selected')
       mainPhoto.innerHTML = `<img src="./assets/${bigPhotos[index]}-big.jpg" alt="Imagem Grande do Tênis"></img>`
       titleProduct.innerHTML = `<h1>Xtreme 200mx - <span class='capitalize'>${colorProduct[index]} </span></h1>`
-    }
+      console.log(colorProduct[index])
+      selectedColor = colorProduct[index]
   })
 })
 
 const addSubNumber = document.querySelectorAll('.payment__amount button')
 const inputNumberQuantity = document.querySelector('.payment__amount input')
 const price = document.querySelector('.main-info__price--real span')
-
-console.log(price.innerText)
 
 inputNumberQuantity.value = 1
 
@@ -116,8 +117,8 @@ function detectNumber(numbers) {
     numbers = 1
     addSubNumber[1].disabled = true
     addSubNumber[0].disabled = false
-  } else if(numbers >= 10) {
-    numbers = 10
+  } else if(numbers >= 3) {
+    numbers = 3
     addSubNumber[1].disabled = false
     addSubNumber[0].disabled = true
   } else {
@@ -137,8 +138,41 @@ addSubNumber.forEach((symbol, index) => {
   symbol.addEventListener('click', () => {
     inputNumberQuantity.value = eval(inputNumberQuantity.value + addSubNumber[index].textContent + 1)
     detectNumber(inputNumberQuantity.value)
-    const setHtml = updatePrice(inputNumberQuantity.value)
-    console.log(setHtml)
-    price.innerText = setHtml
+    selectedQuantity = inputNumberQuantity.value
+    selectedPrice = updatePrice(inputNumberQuantity.value)
   })
+})
+
+const btnBuy = document.querySelector('.payment__button button')
+
+btnBuy.addEventListener('click', () => {
+  console.log(selectedColor)
+  console.log(selectedSize)
+  console.log(selectedQuantity)
+  console.log(selectedPrice)
+  
+  const product = {
+    color: selectedColor,
+    size: selectedSize,
+    quantity: selectedQuantity,
+    price: selectedPrice
+  }
+
+  const jsonProduct = JSON.stringify(product, null, 2)
+
+  console.log(jsonProduct)
+
+  const blob = new Blob([jsonProduct], { type: 'application/json'})
+
+  const url = URL.createObjectURL(blob)
+  const link = document.createElement('a')
+  link.href = url
+  link.download = 'product.json'
+
+  document.body.appendChild(link)
+  link.click()
+
+  document.body.removeChild(link)
+  URL.revokeObjectURL(url)
+
 })
